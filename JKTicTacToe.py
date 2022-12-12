76f834b727b01285dd661c0544fe4c782b4c7bd9
Jayakanth Arun <jayakantharun03@gmail.com>
2022-12-12 12:04:37 +0530

    Updated Sound

--- JKTicTacToe.py
+++ JKTicTacToe.py
@@ -1,12 +1,15 @@
 from tkinter import *
 from tkinter import messagebox,filedialog
 from PIL import ImageTk,Image
+import pygame
 
 def quitgame():
+    pygame.mixer.Sound.play(buttonclicksound)
     messagebox.showinfo("MESSAGE","THANK YOU FOR PLAYING")
-    top.destroy()
+    exit()
 
 def startgame():
+    pygame.mixer.Sound.play(buttonclicksound)
     global win,count,flag,button1flag,button2flag,button3flag,button4flag,button5flag,button6flag,button7flag,button8flag,button9flag
     root=Tk()
 
@@ -29,22 +32,25 @@ def startgame():
     def checkwin():
         global win
         if (button1['text']=="X" and button2['text']=="X" and button3['text']=="X") or (button4['text']=="X" and button5['text']=="X" and button6['text']=="X") or (button7['text']=="X" and button8['text']=="X" and button9['text']=="X") or (button1['text']=="X" and button4['text']=="X" and button7['text']=="X") or (button2['text']=="X" and button5['text']=="X" and button8['text']=="X") or (button3['text']=="X" and button6['text']=="X" and button9['text']=="X") or (button1['text']=="X" and button5['text']=="X" and button9['text']=="X") or (button3['text']=="X" and button5['text']=="X" and button7['text']=="X"):
+            pygame.mixer.Sound.play(winsound)
             messagebox.showinfo("MESSAGE","PLAYER 1 WINS",master=root)
             win=1
             root.destroy()
         elif (button1['text']=="O" and button2['text']=="O" and button3['text']=="O") or (button4['text']=="O" and button5['text']=="O" and button6['text']=="O") or (button7['text']=="O" and button8['text']=="O" and button9['text']=="O") or (button1['text']=="O" and button4['text']=="O" and button7['text']=="O") or (button2['text']=="O" and button5['text']=="O" and button8['text']=="O") or (button3['text']=="O" and button6['text']=="O" and button9['text']=="O") or (button1['text']=="O" and button5['text']=="O" and button9['text']=="O") or (button3['text']=="O" and button5['text']=="O" and button7['text']=="O"):
+            pygame.mixer.Sound.play(winsound)
             messagebox.showinfo("MESSAGE","PLAYER 2 WINS",master=root)
             win=1
             root.destroy()    
 
     def checkdraw():
         if count==9 and win==0:
+            pygame.mixer.Sound.play(drawsound)
             messagebox.showinfo("MESSAGE","GAME DRAW",master=root)
             root.destroy()
 
     def buttonclick1():
         global flag,button1flag,count
-        
+        pygame.mixer.Sound.play(clicksound)
         if flag==0 and button1flag==0:
             button1['text']="X"
             button1flag=1
@@ -56,12 +62,13 @@ def startgame():
             flag=0
             count+=1
         else:
+            pygame.mixer.Sound.play(errorsound)
             messagebox.showerror("ERROR","INVALID OPERATION",master=root)
         checkwin()    
         checkdraw()    
     def buttonclick2():
         global flag,button2flag,count
-        
+        pygame.mixer.Sound.play(clicksound)
         if flag==0 and button2flag==0:
             button2['text']="X"
             button2flag=1
@@ -73,12 +80,13 @@ def startgame():
             flag=0
             count+=1
         else:
+            pygame.mixer.Sound.play(errorsound)
             messagebox.showerror("ERROR","INVALID OPERATION",master=root)
         checkwin()
         checkdraw() 
     def buttonclick3():
         global flag,button3flag,count
-        
+        pygame.mixer.Sound.play(clicksound)
         if flag==0 and button3flag==0:
             button3['text']="X"
             button3flag=1
@@ -90,13 +98,14 @@ def startgame():
             flag=0
             count+=1
         else:
+            pygame.mixer.Sound.play(errorsound)
             messagebox.showerror("ERROR","INVALID OPERATION",master=root)
         
         checkwin()
         checkdraw() 
     def buttonclick4():
         global flag,button4flag,count
-        
+        pygame.mixer.Sound.play(clicksound)
         if flag==0 and button4flag==0:
             button4['text']="X"
             button4flag=1
@@ -108,12 +117,13 @@ def startgame():
             flag=0
             count+=1
         else:
+            pygame.mixer.Sound.play(errorsound)
             messagebox.showerror("ERROR","INVALID OPERATION",master=root)  
         checkwin()
         checkdraw()                                
     def buttonclick5():
         global flag,button5flag,count
-        
+        pygame.mixer.Sound.play(clicksound)
         if flag==0 and button5flag==0:
             button5['text']="X"
             button5flag=1
@@ -125,12 +135,13 @@ def startgame():
             flag=0
             count+=1
         else:
+            pygame.mixer.Sound.play(errorsound)
             messagebox.showerror("ERROR","INVALID OPERATION",master=root)
         checkwin()
         checkdraw()     
     def buttonclick6():
         global flag,button6flag,count
-        
+        pygame.mixer.Sound.play(clicksound)
         if flag==0 and button6flag==0:
             button6['text']="X"
             button6flag=1
@@ -142,12 +153,13 @@ def startgame():
             flag=0
             count+=1
         else:
+            pygame.mixer.Sound.play(errorsound)
             messagebox.showerror("ERROR","INVALID OPERATION",master=root)
         checkwin()
         checkdraw()     
     def buttonclick7():
         global flag,button7flag,count
-        
+        pygame.mixer.Sound.play(clicksound)
         if flag==0 and button7flag==0:
             button7['text']="X"
             button7flag=1
@@ -159,12 +171,13 @@ def startgame():
             flag=0
             count+=1
         else:
+            pygame.mixer.Sound.play(errorsound)
             messagebox.showerror("ERROR","INVALID OPERATION",master=root)  
         checkwin()
         checkdraw()                           
     def buttonclick8():
         global flag,button8flag,count
-        
+        pygame.mixer.Sound.play(clicksound)
         if flag==0 and button8flag==0:
             button8['text']="X"
             button8flag=1
@@ -176,12 +189,13 @@ def startgame():
             flag=0
             count+=1
         else:
+            pygame.mixer.Sound.play(errorsound)
             messagebox.showerror("ERROR","INVALID OPERATION",master=root)
         checkwin()
         checkdraw()     
     def buttonclick9():
         global flag,button9flag,count
-        
+        pygame.mixer.Sound.play(clicksound)
         if flag==0 and button9flag==0:
             button9['text']="X"
             button9flag=1
@@ -193,6 +207,7 @@ def startgame():
             flag=0
             count+=1
         else:
+            pygame.mixer.Sound.play(errorsound)
             messagebox.showerror("ERROR","INVALID OPERATION",master=root)  
         checkwin()
         checkdraw()                    
@@ -223,6 +238,13 @@ def startgame():
     root.mainloop()
 
 top=Tk()
+
+pygame.init()
+clicksound=pygame.mixer.Sound("click.wav")
+drawsound=pygame.mixer.Sound("draw.wav")
+winsound=pygame.mixer.Sound("win.wav")
+buttonclicksound=pygame.mixer.Sound("buttonclick.wav")
+errorsound=pygame.mixer.Sound("error.wav")
 
 top.title("JK TIC TAC TOE")
 
